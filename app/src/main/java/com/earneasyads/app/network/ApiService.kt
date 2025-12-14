34c8da70abf121527a8b3fa014886b0367ef0d36
FrankrazyToady <franciselimlim06@gmail.com>
2025-12-15 02:26:45 +0300

    Update ApiService.kt

--- app/src/main/java/com/earneasyads/app/network/ApiService.kt
+++ app/src/main/java/com/earneasyads/app/network/ApiService.kt
@@ -2,6 +2,8 @@ package com.earneasyads.app.network
 
 import com.earneasyads.app.models.LoginRequest
 import com.earneasyads.app.models.LoginResponse
+import com.earneasyads.app.models.SignupRequest
+import com.earneasyads.app.models.SignupResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -11,7 +13,6 @@ interface ApiService {
     @POST("api/auth/login")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
-    // Optional: Signup API will be added later
-    // @POST("api/auth/signup")
-    // fun signup(@Body signupRequest: SignupRequest): Call<SignupResponse>
+    @POST("api/auth/signup")
+    fun signup(@Body signupRequest: SignupRequest): Call<SignupResponse>
 }
